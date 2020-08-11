44fd865e28835f04a2b1e7e4c2678d9035d29184
Thomas <thomaz.linden@gmail.com>
2020-08-11 11:36:35 +0200

    circumvent xgboost, no conda setup

--- johann-first.sh
+++ johann-first.sh
@@ -1,13 +1,22 @@
 mkdir delme
 cd delme
 
+
+# --- @Johann need edit: start 
 # @Johann: please provide path (from *within* the container) to those two files
-H5FULLPATH= # maybe H5FULLPATH="/root/work/data/mh1_fu1_CNN1D_subdomain.h5"
-CVFULLPATH= # maybe CVFULLPATH="/root/work/data/censored.incident.cvsamp.csv"
+# find . -name "mh1_fu1_CNN1D_subdomain.h5"
+# find . -name "censored.incident.cvsamp.csv"
+H5FULLPATH=$(find . -name "mh1_fu1_CNN1D_subdomain.h5")
+CVFULLPATH=$(find . -name "censored.incident.cvsamp.csv")
+
+# example 
+#H5FULLPATH=E610046/TGDL-data/data/mh1_fu1_CNN1D_subdomain.h5 # 
+#CVFULLPATH=E610046/TGDL-data/data/censored.incident.cvsamp.csv # 
+# --- @Johann need edit: stop
 
 # download + install project and shap_explainer package
-git clone https://gitlab.com/tlinden/shap_explainer.git
-cd shap_explainer
+git clone https://github.com/thomasmooon/shap_explainer_github.git
+cd shap_explainer_github
 
 # copy files
 cp $H5FULLPATH data/processed/
